267a21b9133676eda797a854fd8d63bcfb43c67f
ChenAiwei <2268967541@qq.com>
2021-04-16 16:11:53 +0800

    33

--- src/main/java/com/design/pattern/Test.java
+++ src/main/java/com/design/pattern/Test.java
@@ -6,5 +6,7 @@ package com.design.pattern;
  * @CreateDate：2021/4/16 16:06
  */
 public class Test {
-
+	public static void main(String[] args) {
+		System.out.println(" = " + System.currentTimeMillis() + "," + "当前类=Test.main()");
+	}
 }
